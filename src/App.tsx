8a67bf54147cff42530067289f0218511845120f
EC2 Default User <ec2-user@ip-10-0-1-95.ec2.internal>
2025-01-29 09:10:37 +0000

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,61 @@
 import { useEffect, useState } from 'react';
-import type { Schema } from '../amplify/data/resource';
-import { generateClient } from '@aws-amplify/api';
+import type { Schema } from './API';
+import { generateClient } from 'aws-amplify/api';
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema['Todo']['Item']>>([]);
+    const [todos, setTodos] = useState<Array<Schema['Todo']['Item']>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    const fetchTodos = async () => {
+        const { data: items, errors } = await client.models.Todo.list();
+        alert(items.length + " todos fetched")
+    };
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt('What would you like to do?') });
-  }
+    useEffect(() => {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }, []);
 
-  return (
-      <Authenticator>
-      {({signOut, user})=> (
-            <main>
-              <h1>My todos</h1>
-              <button onClick={createTodo}>+ new</button>
-              <ul>
-                {todos.map((todo) => (
-                  <li key={todo.id}>{todo.content}</li>
-                ))}
-              </ul>
-              <div>
-                🥳 App successfully hosted. Try creating a new todo.
-                <br />
-                <a href="https://docs.amplify.aws/gen2/start/quickstart/">
-                  Review next step of this tutorial.
-                </a>
-              </div>
-              <div>
-                <button onClick={signOut}>Sign out</button>
-              </div>
-            </main>
-        )}
-    </Authenticator>
-  );
+    function createTodo() {
+        client.models.Todo.create({ content: window.prompt("What do you need to do?") });
+    }
+
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
+    return (
+        <Authenticator>
+            {({signOut, user})=> (
+                <main>
+                    <h1> {user?.signInDetails?.loginId}'s todos</h1>
+                    <button onClick={createTodo}>+ new</button>
+                    <ul>
+                        {todos.map((todo) => (
+                            <li
+                                onClick={() => deleteTodo(todo.id)}
+                                key={todo.id}>{todo.content}</li>
+                        ))}
+                    </ul>
+                    <div>
+                        🥳 App successfully hosted. Try creating a new todo.
+                        <br />
+                        <a href="https://docs.amplify.aws/gen2/start/quickstart/">
+                            Review next step of this tutorial.
+                        </a>
+                    </div>
+                    <div>
+                        <button onClick={fetchTodos}>Fetch Data</button>
+                        <button onClick={signOut}>Sign out</button>
+                    </div>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "absproxy/5173"
+  // base : "absproxy/5173"
 })
